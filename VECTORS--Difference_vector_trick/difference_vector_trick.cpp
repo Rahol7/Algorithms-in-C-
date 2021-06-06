a7d77237c8524816fc5a8817b582ec29e00a11ed
Rahol1 <5raholsaha@gmail.com>
2021-06-06 16:28:43 +0530

    added comments about the question and possible test cases

--- VECTORS--Difference_vector_trick/difference_vector_trick.cpp
+++ VECTORS--Difference_vector_trick/difference_vector_trick.cpp
@@ -1,4 +1,8 @@
+/*
+Question: Given an array of size n initialised with all 0's. You have 'Q' queries. In each query you get two numbers 'L' and 'R'. Increase all values 
+of the array from L to R by 1 in each of the Q queries. Print the output
 
+*/
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -32,3 +36,17 @@ int main() {
 	}	
 }
 
+/* Run this test case
+
+input format
+7 //size of the array
+3 // no of queries
+2 4 // q1
+1 5 // q2
+3 5 //q3
+
+output 0 1 2 3 3 2 0
+
+
+
+*/
